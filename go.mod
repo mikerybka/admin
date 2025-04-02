f29c87a68221431fb1842a4f82cedbe413ab9ae9
Mike Rybka <merybka@gmail.com>
2025-04-02 16:07:26 -0400

    Create admin server

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mikerybka/admin
+
+go 1.24.2
---
+++ main.go
@@ -0,0 +1,32 @@
+package main
+
+import (
+	"io"
+	"net/http"
+	"strings"
+
+	"github.com/mikerybka/twilio"
+	"github.com/mikerybka/util"
+)
+
+func main() {
+	twilioClient := twilio.NewClient(
+		util.RequireEnvVar("TWILIO_ACCOUNT_SID"),
+		util.RequireEnvVar("TWILIO_AUTH_TOKEN"),
+		util.RequireEnvVar("TWILIO_PHONE_NUMBER"),
+	)
+	adminPhone := util.RequireEnvVar("ADMIN_PHONE_NUMBER")
+	http.HandleFunc("POST /alert", func(w http.ResponseWriter, r *http.Request) {
+		b, err := io.ReadAll(r.Body)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+		err = twilioClient.SendSMS(adminPhone, strings.TrimSpace(string(b)))
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+	})
+	http.ListenAndServe(":2222", nil)
+}
